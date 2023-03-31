0ae0d061aa892a267868cc18f39781689476983e
Your Name <you@example.com>
2023-03-31 16:59:07 +0800

    加上favicon

--- client/components/layout.js
+++ client/components/layout.js
@@ -37,6 +37,7 @@ export default function Layout({children}){
       <meta charSet="utf-8"/>
       <meta name="viewport" content="width=device-width,initial-scale=1"/>
       <title>{websiteTitle}</title>
+      <link rel="icon" href="/favicon.ico" />
       <style>
 
       </style>
